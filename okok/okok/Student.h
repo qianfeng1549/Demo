46625f90f4d5a1dd9d793a76538ed2691009d996
ccw88 <328592171@qq.com>
2015-11-23 11:09:52 +0800

    laowang

---
+++ okok/okok/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  okok
+//
+//  Created by OnePiece on 15/11/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ okok/okok/Student.m
@@ -0,0 +1,14 @@
+
+//
+//  Student.m
+//  okok
+//
+//  Created by OnePiece on 15/11/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
